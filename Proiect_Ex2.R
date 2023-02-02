5fdd029bebd763e8a5d2a30ee2ad6e53fb30d23e
Radu Tomescu <radut4321@gmail.com>
2023-02-02 21:26:22 +0200

    Update Proiect_Ex2.R

--- Proiect_Ex2.R
+++ Proiect_Ex2.R
@@ -1,40 +1,69 @@
 f <- function (x,a=1){
   return(a*(sin(x)^2*exp(-x^2*sqrt(x))))
 }
-g1 <- function(x){
-  return(1/2 *exp(-x))
-}
 
 g2 <- function(x){
   return(1/(2*pi) * 1/(1+ (x^2)/4))
 }
 
+
+rg2 <- function(n){
+   # a se completa 
+}
+
+
 fmed<-function(x){
-  return(x*f(x,3.852985))
+  return(x*f(x,3.852985))  # folosita pt a calcula media
 }
 
-raport<-function(x){
-  return(f(x)/dnorm(x)) 
-}
-
-raportbis<-function(x){
-  return(f(x,3.852985)/dnorm(x)) 
-}
 
 raport1<-function(x){
-  return(f(x)/g1(x)) 
+  return(f(x)/dexp(x))  
 }
 
 raport2<-function(x){
   return(f(x)/g2(x)) 
 }
 
-#print(integrate(f,0,Inf,3.852985))
+raport3<-function(x){
+  return(f(x)/(2*dnorm(x))) 
+}
 
 medie_exacta<-integrate(fmed,0,Inf)$value
-#print(medie_exacta)
 
-func1 <- function(dummy, k=2)
+
+
+func1 <- function(dummy, k=1)
+{
+  while (TRUE)
+  { 
+    u <- runif(1)
+    y <- rexp(1)
+    while(y<0){
+      y<-rnorm(1)
+    }
+    
+    g <- dexp(y)
+    if (u <= f(y) / (k* g)) return(y) 
+  }
+}
+
+
+freq1 <- function(dummy, k=1)
+{
+  u <- runif(1)
+  y <- rexp(1)
+  while(y<0){
+    y<-rnorm(1)
+  }
+  g1 <- dexp(y)
+  if (u <= f(y) / (k* g1)) return(1) 
+  return(0)
+}
+
+
+
+func3 <- function(dummy, k=1)
 {
   while (TRUE)
   { 
@@ -44,13 +73,17 @@ func1 <- function(dummy, k=2)
       y<-rnorm(1)
     }
     
-    g1 <- 2*dnorm(y)
-    if (u <= f(y) / (k* g1)) return(y) 
+    g <- 2*dnorm(y)
+    if (u <= f(y) / (k* g)) return(y) 
   }
 }
 
+aux3 <- function (nr,rez){
+ 
+  return(mean(rez[1:nr]))
+}
 
-alt1 <- function(dummy, k=1)
+freq3<- function(dummy, k=1)
 {
     u <- runif(1)
     y <- rnorm(1)
@@ -58,48 +91,52 @@ alt1 <- function(dummy, k=1)
       y<-rnorm(1)
     }
     g1 <- 2*dnorm(y)
-    if (u <= f(y,3.852985) / (k* g1)) return(1) 
+    if (u <= f(y) / (k* g1)) return(1) 
     return(0)
 }
 
 
-x<-seq(0,4,0.01)
+x<-seq(0,4,0.001)  # intervalul discretizat
 
-sup<-optimise(raport,x,maximum=TRUE)
-supbis<-optimise(raportbis,x,maximum=TRUE)
+
 sup1<-optimise(raport1,x,maximum=TRUE)
 sup2<-optimise(raport2,x,maximum=TRUE)
+sup3<-optimise(raport3,x,maximum=TRUE)
+
+
+acceptate1<-sapply(1:10^5,freq1,sup1$objective)
+acceptate3<-sapply(1:10^5,freq3,sup3$objective)
+
+rata1<-mean(acceptate1)
+rata3<-mean(acceptate3)
+
+constAprox1<-1/((sup1$objective)*rata1)
+constAprox3<-1/((sup3$objective)*rata3)
 
 
 
-acceptate<-sapply(1:10^5,alt1,supbis$objective)
+rez1<-sapply(1:10^5,func1,sup1$objective)
+rez3<-sapply(1:10^6,func3,sup3$objective)
 
-rata<-mean(acceptate)
+medie3<-mean(rez3)
+
+medii3<-cumsum(rez3)/seq_along(rez3)
+
+
+criteriu<-which(abs(medii3-medie_exacta)<=0.001)
+
+print(criteriu)
+
+hist(rez1,freq=F)
+
+lines(x,f(x,constAprox1),col='red')
+
+
+hist(rez3,freq=F)
+
+lines(x,f(x,constAprox3),col='red')
 
 
 
 
 
-
-plot(x,f(x,3.852985),type='l',col='blue')
-lines(x,sup$objective*dnorm(x))
-lines(x,supbis$objective*dnorm(x))
-lines(x,f(x),col='red')
-lines(x,sup1$objective*g1(x), col="green")
-lines(x,sup2$objective*g2(x), col="pink")
-
-rez<-sapply(1:10^5,func1,sup$objective)
-
-maxim_simulat<-optimise(f,x,maximum = TRUE)$objective
-print(maxim_simulat*1/sup$objective)
-
-medie<-mean(rez)
-#print(medie)
-hist(rez,freq=F)
-lines(x,f(x,3.852985),col='blue')
-lines(x,f(x))
-lines(medie,f(medie),type='p')
-lines(medie,f(medie,3.852985),type='p',col='blue')
-lines(x,f(x,sup$objective/f(medie
-                            )),col='red')
-# lines(x,sup$objective*dnorm(x))
